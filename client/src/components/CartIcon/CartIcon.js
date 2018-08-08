53d4f1c64a0fec47bb898689b147b80c9fab514c
William <seewilliam.gao@gmail.com>
2018-08-07 18:20:08 -0700

    adjusted cart icon position

--- client/src/components/CartIcon/CartIcon.js
+++ client/src/components/CartIcon/CartIcon.js
@@ -4,8 +4,8 @@ import CartImage from "../../assets/images/cart.png"
 
 const Cart = ({cartCount}) => (
 
-        <div>
-            <a href="/cart"><img id="cart" src={CartImage} height="40px" width="40" alt="cart"/></a>
+        <div id="carContainer">
+            <a href="/cart"><img id="cart" src={CartImage} height="30px" width="30" alt="cart"/></a>
             <a href="/cart"><p id="cartCount">({cartCount})</p></a>
         </div>
 )
