0aa98a5c1f07eb2232acbbd1548334cfb442e8a5
William <seewilliam.gao@gmail.com>
2018-08-11 14:14:04 -0700

    adjusted checkout page and course page '

--- client/src/components/Form/PlainInput.js
+++ client/src/components/Form/PlainInput.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 export const PlainInput = props => (
-  <div className="form-group pt-5">
+  <div className="form-group pt-2">
     <label className="label-float" style={{color: "black", fontSize: "1em"}}>{props.label}</label>
-    <input className="form-control" {...props}/>
+    <br/>
+    <input className="form-control mt-1" {...props}/>
   </div>
 );
